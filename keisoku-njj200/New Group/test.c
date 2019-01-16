f5cb276a202e7f68918f6a5ccecfbe694d1f92e0
thuzar <mya.thu.zar@arise-inc.net>
2019-01-16 16:12:35 +0630

    testing bridging header.h

---
+++ keisoku-njj200/New Group/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  keisoku-njj200
+//
+//  Created by arise-M008 on 1/16/19.
+//  Copyright © 2019 jp.co.kgs-inc. All rights reserved.
+//
+
+#include "test.h"
---
+++ keisoku-njj200/New Group/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  keisoku-njj200
+//
+//  Created by arise-M008 on 1/16/19.
+//  Copyright © 2019 jp.co.kgs-inc. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */
